be900bfeb8dedb7ad42896d5ecada327a895a7d2
Michael <michael.mr.roth@live.com>
2025-02-05 23:39:29 +0100

    renaming

--- mitoSoft.homeNet.ArduinoIDE/Form1.Designer.cs
+++ mitoSoft.homeNet.ArduinoIDE/Form1.Designer.cs
@@ -68,9 +68,9 @@
             label1.AutoSize = true;
             label1.Location = new Point(12, 10);
             label1.Name = "label1";
-            label1.Size = new Size(137, 32);
+            label1.Size = new Size(407, 32);
             label1.TabIndex = 1;
-            label1.Text = "YAML code:";
+            label1.Text = "Home Assistant - configuration.yaml:";
             // 
             // menuStrip1
             // 
